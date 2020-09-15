e5adc7e8e84a0d84ba41faefb967216e5b1be23b
a-Ksy <aaksoy17@ku.edu.tr>
2020-09-15 17:48:42 +0300

    CreateRoom page ui

--- frontend/src/pages/CreateRoom/CreateRoom.jsx
+++ frontend/src/pages/CreateRoom/CreateRoom.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import Page from '../../components/Page/Page';
@@ -7,8 +8,17 @@ import './CreateRoom.scss';
 function CreateRoom() {
   return (
     <Page>
-      <div className="Landing">
-        <h1>create room page</h1>
+      <div className="CreateRoom">
+        <div className="card">
+          <div className="card-body">
+            <h4 className="title">Welcome to code names</h4>
+            <p className="subtitle">To enter a room, choose a nickname.</p>
+            <label className="label">Nickname</label>
+            <input type="text" id="nicknameInput" placeholder="Enter your nickname" />
+            <br />
+            <Button title="create room" />
+          </div>
+        </div>
       </div>
     </Page>
   );
